1b8ef4763fd6049dd6f909152bc61b0b393e9553
Igor K. <kia1809@yandex.ru>
2026-09-08 18:20:14 +0300

    add database pacage

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from __future__ import annotations
 import flet as ft
-import asyncio
 
 from src.gui_flet.main_window import MainWindow
 
 
 def setup_window(page: ft.Page):
-    page.window.width = 1600
-    page.window.height = 1200
+    page.window.width = 1024
+    page.window.height = 800
     page.title = "Рабочий помощник КИА"
     page.window.title_bar_hidden = True
     page.run_task(page.window.center)
